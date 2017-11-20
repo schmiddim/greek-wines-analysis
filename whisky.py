bde5835eed4281e5d082aaed73eec347269033a2
Michael Schmitt <mschmitt@mybestbrands.de>
2017-11-20 14:06:50 +0100

    further fields

--- whisky.py
+++ whisky.py
@@ -1,7 +1,10 @@
 from whisky_de import get
+from whisky_de.scraper import WhiskyScraper
 
+
+#print(WhiskyScraper().number_of_whiskys)
 
 url='https://www.whisky.de/shop/Schottland/Single-Malt/Islay/Lagavulin-8-Jahre.html'
-url='https://www.whisky.de/shop/USA/Rye-Corn-Andere/Jim-Beam-Rye-inkl-gratis-Glas.html'
+#url='https://www.whisky.de/shop/USA/Rye-Corn-Andere/Jim-Beam-Rye-inkl-gratis-Glas.html'
 
 print(get(url))
--- whisky_de/scraper.py
+++ whisky_de/scraper.py
@@ -30,21 +30,39 @@ class WhiskyScraper:
             'price': int(find_non_empty('span', class_='article-price-default').replace('EUR', '').replace(',', '')),
             'whisky_type': soup.select("div.productMainInfo  > div.article-attributes  > ul > li")[0].text.replace(
                 'Sorte:', '').strip(),
-            'img' : soup.find( id='image-zoom-first-image').get('src')
+            'img': soup.find(id='image-zoom-first-image').get('src')
+            'distillery': find_non_empty('div',  class_= 'article-company').strip()
+
         })
 
         article_amounts = soup.find('div', class_='article-amount').text.strip().split('\n')
         ret['alcohol_%'] = float(article_amounts[1].replace('% vol', '').strip())
 
 
-        #tasteboxes
-        ret['taste_box_sherry_port_barrel'] =  soup.find(id="tastevote_criteriainput_0").get('value')
-        ret['taste_box_smoke_torf'] =  soup.find(id="tastevote_criteriainput_1").get('value')
-        ret['taste_box_sweet'] =  soup.find(id="tastevote_criteriainput_2").get('value')
-        ret['taste_box_fruits'] =  soup.find(id="tastevote_criteriainput_3").get('value')
-        ret['taste_box_abgang'] =  soup.find(id="tastevote_criteriainput_4").get('value')
-        ret['taste_box_gesamt'] =  soup.find(id="tastevote_criteriainput_5").get('value')
-        ret['taste_box_qualitiy'] =  soup.find(id="tastevote_criteriainput_6").get('value')
+        #Country
+        items =soup.find('form', id='cloud-zoom-width').findAll('div' , class_="list-csv")
+        ret['country'] = items[1].text.replace('Herkunftsland:', '').strip().split('\n') [1]
 
+
+        ret['taste_box_sherry_port_barrel'] = soup.find(id="tastevote_criteriainput_0").get('value')
+        ret['taste_box_smoke_torf'] = soup.find(id="tastevote_criteriainput_1").get('value')
+        ret['taste_box_sweet'] = soup.find(id="tastevote_criteriainput_2").get('value')
+        ret['taste_box_fruits'] = soup.find(id="tastevote_criteriainput_3").get('value')
+        ret['taste_box_gravy'] = soup.find(id="tastevote_criteriainput_4").get('value')
+        ret['taste_box_total'] = soup.find(id="tastevote_criteriainput_5").get('value')
+        ret['taste_box_qualitiy'] = soup.find(id="tastevote_criteriainput_6").get('value')
 
         return ret
+
+    @property
+    def number_of_whiskys(self):
+        bla = int(
+            BeautifulSoup(self._session.get('http://www.houseofwine.gr/how/wine.html?mode=list').content, 'lxml'
+                          ).find('p',
+                                 class_='amount'
+                                 ).text.replace('\n', '').replace('\r', '').lstrip(' ').rstrip(' ').split(' ')[
+                -2])  # Είδη 1 εώς 25 από 1123 σύνολο
+        return bla
+        # def pages_urls(selfself, page_size=30):
+        #     page_urls = set()
+        #     n_pages= self.number_of_
